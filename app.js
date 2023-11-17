0def0b733129c4316ccb401e3879ea8dd0f038a4
Varshitha <varshitha.lavu@gmail.com>
2023-11-17 12:25:15 -0600

    added

--- app.js
+++ app.js
@@ -5,14 +5,36 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var Scoop = require("./models/scoop");
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
 require('dotenv').config();
 
 const connectionString =
-process.env.MONGO_CON
+  process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString,
-{useNewUrlParser: true,
-useUnifiedTopology: true});
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -33,14 +55,24 @@ app.set('view engine', 'pug');
 var db = mongoose.connection;
 //Bind connection to error event 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -50,13 +82,21 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -67,34 +107,37 @@ app.use(function(err, req, res, next) {
 });
 
 // We can seed the collection if needed on server start
-async function recreateDB(){
- // Delete everything
- await Scoop.deleteMany();
- let instance1 = new Scoop({scoopFlavor: 'Vanilla', scoopSize: 'Small', scoopColor: 'White', scoopPrice: 2.50 });
-//  let instance2 = new Scoop({ scoopFlavor: 'Vanilla', scoopSize: 'Small', scoopColor: 'White', scoopPrice: 2.50 });
- let instance3 = new Scoop({scoopFlavor: 'Chocolate', scoopSize: 'Medium', scoopColor: 'Brown', scoopPrice: 3.00});
- let instance4 = new Scoop({scoopFlavor: 'Strawberry', scoopSize: 'Large', scoopColor: 'Pink', scoopPrice: 3.50});
- instance1.save().then(doc=>{
- console.log("First object saved")}
- ).catch(err=>{
- console.error(err)
- });
-//  instance2.save().then(doc=>{
-//   console.log("Second object saved")}
-//   ).catch(err=>{
-//   console.error(err)
-//   });
-  instance3.save().then(doc=>{
-    console.log("Third object saved")}
-    ).catch(err=>{
+async function recreateDB() {
+  // Delete everything
+  await Scoop.deleteMany();
+  let instance1 = new Scoop({ scoopFlavor: 'Vanilla', scoopSize: 'Small', scoopColor: 'White', scoopPrice: 2.50 });
+  //  let instance2 = new Scoop({ scoopFlavor: 'Vanilla', scoopSize: 'Small', scoopColor: 'White', scoopPrice: 2.50 });
+  let instance3 = new Scoop({ scoopFlavor: 'Chocolate', scoopSize: 'Medium', scoopColor: 'Brown', scoopPrice: 3.00 });
+  let instance4 = new Scoop({ scoopFlavor: 'Strawberry', scoopSize: 'Large', scoopColor: 'Pink', scoopPrice: 3.50 });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
     console.error(err)
-    });
-    instance4.save().then(doc=>{
-      console.log("Fourth object saved")}
-      ).catch(err=>{
-      console.error(err)
-      });
+  });
+  //  instance2.save().then(doc=>{
+  //   console.log("Second object saved")}
+  //   ).catch(err=>{
+  //   console.error(err)
+  //   });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+  instance4.save().then(doc => {
+    console.log("Fourth object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 }
 let reseed = true;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 module.exports = app;
